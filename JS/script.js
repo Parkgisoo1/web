ef5c431fc420d29ba103ec1c5c6c6278c71b04c2
Parkgisoo1 <121483339+Parkgisoo1@users.noreply.github.com>
2022-12-27 19:28:15 +0900

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+function inform(){
+    alert("나는 alert입니다")
+}
+function grade(){
+   alert("성적이 확인 되었습니다") 
+}
+function grade(){
+    var kor1 = parseInt(document.getElementById("kor1").innerHTML);
+    var kor2 = parseInt(document.getElementById("kor2").innerHTML);
+    first.innerHTML=kor1+kor2;
+    first.style.color="darkblue";
+    var eng1 = parseInt(document.getElementById("eng1").innerHTML);
+    var eng2 = parseInt(document.getElementById("eng2").innerHTML);
+    var first = document.getElementById(first_g);
+    first.innerHTML=kor1+kor2;
+    first.style.color="darkblue";
+    var mat1 = parseInt(document.getElementById("mat1").innerHTML);
+    var mat2 = parseInt(document.getElementById("mat2").innerHTML);
+    
+}
